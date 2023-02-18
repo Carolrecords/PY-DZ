3755999c2b3fd0cf434f55f141778f3d82e1b511
korolev_as <antonykas@mail.ru>
2023-02-18 23:37:38 +0300

    добавил комментарий к задаче от проверяющего  задач и удалил лишние файлы

--- 51.py
+++ 51.py
@@ -9,8 +9,58 @@
 def power(number, degree):
     if (degree == 1):
         return (number) 
-    if (degree != 1):
+    elif (degree != 1):
         return (number * power(number, degree - 1))
 number = int(input("Введите любое число: "))
 degree = int(input("Введите его степень: "))
 print("Результат возведения числа в степень равен:", power(number, degree))
+
+# ______________________________________________________________________________________
+# Решения отличные)
+# В задаче 51 код:
+
+# if (degree == 1):
+# return (number)
+# if (degree != 1):
+# return (number * power(number, degree - 1))
+
+# # лучше реализовывать или через elif
+
+# if (degree == 1):
+# return (number)
+# elif (degree != 1):
+# return (number * power(number, degree - 1))
+
+# или else:
+
+# if (degree == 1):
+# return (number)
+# else:
+# return (number * power(number, degree - 1))
+
+# Cтарайтесь вместо набора if использовать связку if elif else. Почему? 
+# Если у нас будет набор условий взаимоисключающих, то при куче if они все могу выполниться, 
+# а переход к elif происходит только если не выполнилось предыдущее значение. 
+# Например:
+
+# age = 18
+# if age < 14:
+# print("Ты несовершеннолетний")
+# elif age < 18:
+# print("ДА у тебя уже есть паспорт")
+# elif age < 21:
+# print("Паспорт есть, но крепкий алкоголь тебе не продадим")
+# else:
+# print("Всё дозволено")
+
+# попробуйте исполнить код и сравнить с решением:
+
+# age = 18
+# if age < 14:
+# print("Ты несовершеннолетний")
+# if age < 18:
+# print("ДА у тебя уже есть паспорт")
+# if age < 21:
+# print("Паспорт есть, но крепкий алкоголь тебе не продадим")
+# else:
+# print("Всё дозволено")
